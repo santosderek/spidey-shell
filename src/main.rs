889e49940765a271ded26964791df0bd54cde686
Derek Santos <derek@dyrenex.com>
2023-11-26 19:56:10 -0500

    Added support for .env

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod user_interface;
 mod ai;
 
+use dotenv::dotenv;
+
 fn main() {
+
+    dotenv().ok();
+
     let mut main_window = user_interface::create_main_window();
 
     main_window.run();
